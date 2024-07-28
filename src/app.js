a701e1749997e1165812a80faae84adf1c256fe4
Rishov Chakraborty <rishon3003@gmail.com>
2024-07-28 18:33:12 +0530

    dd

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+app.get('/api/v1/test', (req, res) => {
+    res.json({ message: 'Test endpoint is working!' });
+  });
 
 //import Routes
 import userRouter from "./routes/user.routes.js";
