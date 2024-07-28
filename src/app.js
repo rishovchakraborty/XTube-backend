dfe914ba06533da5bed4c874169cc03bb0c149b0
Rishov Chakraborty <rishon3003@gmail.com>
2024-07-28 18:14:10 +0530

    add

--- src/app.js
+++ src/app.js
@@ -4,10 +4,7 @@ import cookieParser from "cookie-parser";
 // import morgan from "morgan";
 // process.env.CORS_ORIGIN
 const app = express();
-app.use(cors({
-  origin: ["http://localhost:5173"]  ,
-  
-}))
+app.use(cors())
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
